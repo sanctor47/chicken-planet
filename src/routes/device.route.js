0221857ce0f5478731ee7ad748eecbb22eae1f22
aser <aser.e.nabil@gmail.com>
2022-09-06 17:05:03 +0200

    Fake Device Routes

--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -8,15 +8,30 @@ const router = express.Router();
 router.get('', deviceController.getAllDevices);
 
 //route to create a new device
-router.post('',  deviceController.newDevice);
+router.post('', deviceController.newDevice);
 
 //route to get a single device by their device id
-router.get('/:_id', deviceAuth, deviceController.getDevice);
+router.get('/id/:_id', deviceAuth, deviceController.getDevice);
 
 //route to update a single device by their device id
-router.put('/:_id', deviceController.updateDevice);
+router.put('/id/:_id', deviceController.updateDevice);
 
 //route to delete a single device by their device id
-router.delete('/:_id', deviceController.deleteDevice);
+router.delete('/id/:_id', deviceController.deleteDevice);
+
+router.post('/fakeLogin', (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({ data: '38239gf3298f923g0f32g823' });
+});
+
+router.post('/fakeSync', (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({ data: 'Ok' });
+});
+
+router.post('/fakeReading', (req, res, next) => {
+  console.log(req.body);
+  res.status(200).json({ data: 'Ok' });
+});
 
 export default router;
