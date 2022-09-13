22c539ed576d11141ae1d565faf2a306abc9c187
aser <aser.e.nabil@gmail.com>
2022-09-13 18:11:06 +0200

    Ecosystem File

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,29 @@
 module.exports = {
-  apps : [{
-    name   : "app1",
-    script : "./app.js"
-  }]
-}
+  apps: [
+    {
+      name: 'chckenPlanet-server',
+      script: './build/index.js',
+      watch: true,
+      env: {
+        APP_PORT: 5000,
+        NODE_ENV: 'development',
+        DATABASE:
+          'mongodb+srv://test:test1010@cluster0.po862zr.mongodb.net/chckenPlanet?retryWrites=true&w=majority',
+        DATABASE_TEST:
+          'mongodb+srv://test:test1010@cluster0.po862zr.mongodb.net/chckenPlanet?retryWrites=true&w=majority',
+        API_VERSION: 'v1',
+        TOKEN_KEY: '23412214521-215213-51325215216543-643643734-7437436243'
+      },
+      env_production: {
+        APP_PORT: 5000,
+        NODE_ENV: 'production',
+        DATABASE:
+          'mongodb+srv://test:test1010@cluster0.po862zr.mongodb.net/chckenPlanet?retryWrites=true&w=majority',
+        DATABASE_TEST:
+          'mongodb+srv://test:test1010@cluster0.po862zr.mongodb.net/chckenPlanet?retryWrites=true&w=majority',
+        API_VERSION: 'v1',
+        TOKEN_KEY: '23412214521-215213-51325215216543-643643734-7437436243'
+      }
+    }
+  ]
+};
